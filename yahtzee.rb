63f4876d3767aa14e87a9fd873db8b97c806986b
Patrick Kennedy <mixelpix@mac.com>
2016-09-02 02:23:25 -0400

    messing around

--- yahtzee.rb
+++ yahtzee.rb
@@ -8,33 +8,36 @@ roll_1 = Array.new(5) {rand(1..6)}
 p roll_1
 
 def ask message
-  # prompting questions for a string response
+  # prompting questions for a STRING response
   print message
   answer = gets.chomp
   return answer
 end
 
 def ask_num message
-  # prompting questions for an integer response
+  # prompting questions for an INTEGER response
   print message
   answer = gets.to_i
   return answer
 end
 
 again = ask "would you like to keep any of these dice? (Y or N): "
+p again
+p again.class
 
+hold = []
 while again == "Y" or "y"
-  hold = []
   keep = ask_num "Enter a number you'd like to keep: "
   p keep
+  p keep.class
 
   if roll_1.include?(keep)
     roll_1.delete_at(roll_1.find_index(keep))
-    hold += [keep]
+    hold.push(keep)
     puts "You are holding onto #{hold}."
     puts "You have #{roll_1} to choose from."
   else
-    again
+    again = ask "would you like to keep any of these dice? (Y or N): "
   end
 end
 
