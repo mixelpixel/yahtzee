52d847d221572a1a7b699e37bae70dd0a1950dfb
Patrick Kennedy <mixelpix@mac.com>
2016-10-18 03:06:00 -0400

    working out the issues with a single die

--- dice.rb
+++ dice.rb
@@ -41,6 +41,8 @@ p Die.new(min=1, max=20).roll
 p Die.new(max=20)
 p Die.new(max=20).roll
 
+puts
+
 class Die2
 
   def initialize(max=6, min=1)
@@ -56,6 +58,76 @@ end
 
 p Die2.new(20)
 p Die2.new(20).roll
+puts
 
 # http://stackoverflow.com/questions/40099433/manipulating-ruby-class-default-values
+# base/sides model
+
+class Die3
+
+  def initialize(base: 1, sides: 6)
+    @base      = base
+    @sides     = sides
+  end
+
+  def roll
+    rand(@base..(@base + (@sides - 1)))
+  end
+end
+
+p Die3.new.roll
+p Die3.new(sides: 20).roll
+
+puts
+
+class Die4
+
+  def initialize(base: 1, sides: 6)
+    @base      = base
+    @sides     = sides
+  end
+
+  def roll
+    @base + rand(@sides)
+  end
+end
+
+p Die4.new.roll
+p Die4.new(sides: 20).roll
+
+puts
+class Die5 # NOTE THAT THIS MAKES THE RANGE TOO LONG BY ONE
+
+  def initialize(base: 1, sides: 6)
+    @base      = base
+    @sides     = sides
+  end
+  
+  def roll
+    rand(@base..@base+@sides)
+  end
+end
+
+p Die5.new.roll
+p Die5.new(base: 10, sides: 6).roll
+
+
+puts
+class Die6
+
+  def initialize(base: 1, sides: 6)
+    @base      = base
+    @sides     = sides
+  end
+  
+  def roll
+    rand(@base...@base+@sides)
+  end
+end
+
+p Die6.new.roll
+p Die6.new(base: 10, sides: 6).roll
+puts
+p Die6.new(:base=>20, :sides=>50).roll         # alternate syntax
+p Die6.new(:sides=>50, :base=>20).roll         # trivial note, order is unimportant
 
