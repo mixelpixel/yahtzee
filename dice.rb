aaf289c4ea18df956541d798ce5a08c8bfb39f50
Patrick Kennedy <mixelpix@mac.com>
2016-10-18 03:51:02 -0400

    working out the issues with a single die

--- dice.rb
+++ dice.rb
@@ -78,56 +78,56 @@ end
 p Die3.new.roll
 p Die3.new(sides: 20).roll
 
-puts
+# puts
 
-class Die4
+# class Die4
 
-  def initialize(base: 1, sides: 6)
-    @base      = base
-    @sides     = sides
-  end
+  # def initialize(base: 1, sides: 6)
+    # @base      = base
+    # @sides     = sides
+  # end
 
-  def roll
-    @base + rand(@sides)
-  end
-end
+  # def roll
+    # @base + rand(@sides)     # NOTE THAT rand(n) starts at 0 and EXCLUDES n per https://ruby-doc.org/core-2.2.0/Random.html
+  # end
+# end
 
-p Die4.new.roll
-p Die4.new(sides: 20).roll
+# p Die4.new.roll
+# p Die4.new(sides: 20).roll
 
-puts
-class Die5 # NOTE THAT THIS MAKES THE RANGE TOO LONG BY ONE
+# puts
+# class Die5 # NOTE THAT THIS MAKES THE RANGE TOO LONG BY ONE
 
-  def initialize(base: 1, sides: 6)
-    @base      = base
-    @sides     = sides
-  end
+  # def initialize(base: 1, sides: 6)
+    # @base      = base
+    # @sides     = sides
+  # end
   
-  def roll
-    rand(@base..@base+@sides)
-  end
-end
+  # def roll
+    # rand(@base..@base+@sides)
+  # end
+# end
 
-p Die5.new.roll
-p Die5.new(base: 10, sides: 6).roll
+# p Die5.new.roll
+# p Die5.new(base: 10, sides: 6).roll
 
 
-puts
-class Die6
+# puts
+# class Die6
 
-  def initialize(base: 1, sides: 6)
-    @base      = base
-    @sides     = sides
-  end
+  # def initialize(base: 1, sides: 6)
+    # @base      = base
+    # @sides     = sides
+  # end
   
-  def roll
-    rand(@base...@base+@sides)
-  end
-end
+  # def roll
+    # rand(@base...@base+@sides)       # NOTE HOW THE EXCLUSIVE triple ... WORKS
+  # end
+# end
 
-p Die6.new.roll
-p Die6.new(base: 10, sides: 6).roll
-puts
-p Die6.new(:base=>20, :sides=>50).roll         # alternate syntax
-p Die6.new(:sides=>50, :base=>20).roll         # trivial note, order is unimportant
+# p Die6.new.roll
+# p Die6.new(base: 10, sides: 6).roll
+# puts
+# p Die6.new(:base=>20, :sides=>50).roll         # alternate syntax
+# p Die6.new(:sides=>50, :base=>20).roll         # trivial note, order is unimportant
 
