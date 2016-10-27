21e3d203a1b5b7d1e481ac3c30447e9107995759
Patrick Kennedy <mixelpix@mac.com>
2016-10-27 01:22:02 -0400

    conditionals

--- lib/conditional.rb
+++ lib/conditional.rb
@@ -11,34 +11,34 @@ $rolls = 3
 $table_dice = []
 $player_dice = []
 
-def ask_str message
-  print message
-  answer_string = gets.chomp.downcase
+# def ask_str message
+  # print message
+  # answer_string = gets.chomp.downcase
+# end
+
+
+puts "Want to play a game of yahtzee? (y/n): "
+y_or_n = gets.chomp.downcase
+if y_or_n == "y"
+  $rolls -= 1
+  $table_dice = Player.new.first_roll
+else exit
 end
 
-def play?
-  y_or_n = ask_str "Want to play a game of yahtzee? (y/n): "
-  if y_or_n == "y"
-    $rolls -= 1
-    $table_dice = Player.new.first_roll
-  else exit
-  end
-end
-
-play?
 
 puts "You rolled #{$table_dice}"
 puts "Remaining rolls: #{$rolls}"
 
 def keep_or_roll
   keepers = []
-  k_or_r = ask_str "Want to (k)eep some dice, or (r)oll again? (k/r): "
+  puts "Want to (k)eep some dice, or (r)oll again? (k/r): "
+  k_or_r = gets.chomp.downcase
   if k_or_r == "k"
     puts "Which dice to keep from #{$table_dice}? e.g. enter '3' or '3, 1, 6': "
     keepers = gets.chomp.split(',').map { |x| x.to_i }
   else
     $rolls -= 1
-    $table_dice = Player.new.first_roll
+    $table_dice = Player.new.first_roll # WRONG NAME...
   end
 
   to_be_removed = []
@@ -57,16 +57,18 @@ def keep_or_roll
     $table_dice.delete_at j unless j.nil?
   end
   
-  puts "keepers: #{keepers}"
-  puts "player dice: #{$player_dice}"
-  puts "to be removed from table_dice #{to_be_removed}"
+  # puts "keepers: #{keepers}"
+  puts "player keeping these dice: #{$player_dice}"
+  # puts "to be removed from table_dice #{to_be_removed}"
   
-  puts "Keeping these dice: #{$player_dice}"
+  # puts "Keeping these dice: #{$player_dice}"
 
 end
 
 keep_or_roll
-if $table_dice == 0
+
+
+if $table_dice.count == 0
   puts "No dice are left."
 else
   puts "Dice on the table #{$table_dice}"
@@ -78,7 +80,25 @@ if $table_dice.count == 0
   puts "want to add these dice #{$player_dice} to your score card? (y/n)"
 elsif $table_dice.count == 1
   puts "want to roll this die #{$table_dice} again? (y/n)"
+  roll_again = gets.chomp.downcase
+  if roll_again == "y"
+    $rolls -=1
+    $table_dice = Array.new($table_dice.count) { rand(1..6) }
+    puts "Dice on the table: #{$table_dice}"
+    puts "Player dice: #{$player_dice}"
+  else
+    puts "Want to add your dice to score card?"
+  end
 elsif $table_dice.count > 1
   puts "Want to roll these dice #{$table_dice} again? (y/n)"
+  roll_again = gets.chomp.downcase
+  if roll_again == "y"
+    $rolls -=1
+    $table_dice = Array.new($table_dice.count) { rand(1..6) }
+    puts "Dice on the table: #{$table_dice}"
+    puts "Player dice: #{$player_dice}"
+  else
+    puts "Want to add your dice to score card?"
+  end
 end
 
